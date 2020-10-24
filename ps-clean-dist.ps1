3adcc35c63dc317002456b77ac82aa5317aaf88f
Andy Blackledge <andyblackledge@hotmail.com>
2020-10-24 18:58:06 +0100

    Got zips building with folders.

--- ps-clean-dist.ps1
+++ ps-clean-dist.ps1
@@ -1,9 +1,13 @@
 Set-Location dist
 
-Get-ChildItem |
-    Select-Object -ExpandProperty FullName |
-    Where-Object {$_ -notlike 'node_modules'} |
-    Remove-Item -force 
+Remove-Item * -Force -Recurse
+
+# Get-ChildItem |
+#     Select-Object -ExpandProperty FullName |
+#     Where-Object {$_ -notlike '*\dist\node_modules'} |
+#     # Write-Output
+#     Remove-Item -Force 
 
 Set-Location ..
+
 Write-Output "dist cleaned"
