c66424a4c1d0c34d6115df3e16527554148af9a2
Andy Blackledge <andyblackledge@hotmail.com>
2020-10-26 22:09:19 +0000

    Tweaks

--- test/affordabilityApiFunction.test.ts
+++ test/affordabilityApiFunction.test.ts
@@ -1,11 +1,11 @@
 import TypedAPIGatewayProxyEvent from '../src/common/TypedAPIGatewayProxyEvent';
-import * as affordabilityApiFunction from '../src/functions/affordabilityApi/affordabilityApiFunction';
+import * as AffordabilityApi from '../src/functions/affordabilityApi/index';
 
 describe('Test lambda', () => {
 
     it('handles something', async () => {
 
-        const request = {
+        const request: AffordabilityApi.Request = {
             creditCardNumber: '012345678912',
             expiryMonth: 12,
             expiryYear: 2020,
@@ -14,10 +14,9 @@ describe('Test lambda', () => {
             amount: 616.00
         };
 
-        const response = await affordabilityApiFunction.handle(new TypedAPIGatewayProxyEvent(request));
+        const response = await AffordabilityApi.handle(new TypedAPIGatewayProxyEvent(request));
 
         console.log(`request: ${JSON.stringify(request)}`);
-        console.log(`request stringfied: ${JSON.stringify(JSON.stringify(request))}`);
         
         console.log(`response: ${JSON.stringify(response)}`);
     });
